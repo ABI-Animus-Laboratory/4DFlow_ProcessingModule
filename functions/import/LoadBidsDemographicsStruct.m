81920194bb53e8e46622ddbbf174c34b8b421bcd
sdem348 <sdem348@aucklanduni.ac.nz>
2024-04-09 15:56:56 +1200

    feat: Better demographics loading for reporting aquisition params. Not needed for pipeline.

--- functions/import/LoadBidsDemographicsStruct.m
+++ functions/import/LoadBidsDemographicsStruct.m
@@ -1,9 +1,6 @@
 % This function will load demographics for your study cohort if they're
 % stored in a BIDS formatted sourcedata directory. It reads the first DICOM
 % file for each subject and collects demographics from DICOM header. 
-
-%Note: The organisation is based on alphabetical file order. If you want to
-%reorganise another way, add functionality.
 %% Initialization
 path2bids='C:\Users\sdem348\Desktop\Dempsey2023MultiRes_Cohort';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -11,22 +8,25 @@ path2bids='C:\Users\sdem348\Desktop\Dempsey2023MultiRes_Cohort';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Or do
 path2subs=fullfile(path2bids,'sourcedata');
 DIR=dir(path2subs);
-Demos=cell([(length(DIR)-2),4]);
+Demos=struct;
 for i=3:length(DIR(:))
     path2patient=fullfile(path2subs,DIR(i).name);
     DIR2=dir(path2patient);
     DIR3=dir(fullfile(path2patient,DIR2(3).name));
     filename=DIR3(3).name;
     INFO=dicominfo(fullfile(fullfile(path2patient,DIR2(3).name),filename));
-    Demos(i-2,1)={DIR(i).name};
-    Demos(i-2,2)={INFO.PatientAge(2:3)};
-    Demos(i-2,3)={INFO.PatientSex};
-    Demos(i-2,4)={INFO.PatientWeight};
-    Demos(i-2,5)={INFO.HeartRate};
-    Demos(i-2,6)={INFO.HeartRate};
-    Demos(i-2,7)={INFO.HeartRate};
-    Demos(i-2,8)={INFO.HeartRate};
-    Demos(i-2,9)={INFO.HeartRate};
-    Demos(i-2,10)={INFO.HeartRate};
+    Demos.Name(i-2,1)={DIR(i).name};
+    Demos.SubName(i-2,1)={DIR(i).name};
+    Demos.Age(i-2,1)=str2num(INFO.PatientAge(2:3));
+    Demos.Sex(i-2,1)={INFO.PatientSex};
+    Demos.Weight(i-2,1)=INFO.PatientWeight;
+    Demos.RepT(i-2,1)=INFO.RepetitionTime;
+    Demos.EchoT(i-2,1)=INFO.EchoTime;
+    Demos.Bandwidth(i-2,1)=INFO.PixelBandwidth;
+    Demos.ImFreq(i-2,1)=INFO.ImagingFrequency;
+    Demos.FlipA(i-2,1)=INFO.FlipAngle;
+    Demos.HR(i-2,1)=INFO.HeartRate;
+    Demos.Venc(i-2,1)=INFO.Private_0019_10cc;
+    Demos.Scale(i-2,1)=INFO.Private_0019_10e2;
 end
 save(fullfile(path2bids,'derivatives','QVT','population','Demographics.mat'),'Demos')
